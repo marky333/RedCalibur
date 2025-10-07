f020562689f9e29d13a4b1eef948fae531ccea31
Martin Rozariyo <rozariyomartin@gmail.com>
2025-10-07 22:38:26 +0530

    Fix syntax and lint errors in API files

--- api/run.py
+++ api/run.py
@@ -1,7 +1,8 @@
 import uvicorn
 
 if __name__ == "__main__":
-    # For local dev, binding to all interfaces is OK. For CI, suppress Bandit warning.
+    # For local dev, binding to all interfaces is OK.
+    # For CI, suppress Bandit warning.
     uvicorn.run(
         "api.app:app",
         host="0.0.0.0",
